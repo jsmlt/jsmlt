2691c33a420980368245160771412854a789920b
Jesper van Engelen <contact@jepps.nl>
2017-09-11 21:42:23 +0200

    Add documentation to randint function

--- src/util/random.js
+++ src/util/random.js
@@ -1,3 +1,9 @@
+/**
+ * Generate a random integer between a lower bound (inclusive) and an upper bound (exclusive)
+ *
+ * @param int a Lower bound (inclusive)
+ * @param int b Upper bound (exclusive)
+ */
 function randint(a, b) {
   return a + Math.floor((b - a) * Math.random());
 }
