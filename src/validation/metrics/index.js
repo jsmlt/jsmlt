e07e047a467cdfb3f0ad5edfa7abfd34b4b85d68
Jesper van Engelen <contact@jepps.nl>
2017-12-01 21:24:44 +0100

    #10 Add AUROC to validation metrics

--- src/validation/metrics/index.js
+++ src/validation/metrics/index.js
@@ -1,4 +1,6 @@
-/* eslint import/prefer-default-export: "off" */
+// Standard imports
+import * as Arrays from '../../util/arrays';
+import * as LinAlg from '../../math/linalg';
 
 /**
  * Evaluate the accuracy of a set of predictions.
@@ -25,4 +27,63 @@ export function accuracy(yTrue, yPred, normalize = true) {
   }
 
   return numCorrect;
+}
+
+/**
+ * Calculate the area under the receiver-operator curve (AUROC) for a set of predictions. Area is
+ * calculated using the Trapezoidal rule.
+ *
+ * @param {Array.<mixed>} yTrue - True labels
+ * @param {Array.<mixed>} yPred - Predicted labels
+ * @return {number} Calculated AUROC
+ */
+export function auroc(yTrue, yPred) {
+  // Sort the prediction probabilities to get a list of all possible thresholds
+  const sortedIndices = Arrays.argSort(yPred, (a, b) => a - b);
+
+  // To find the false positive rate and true positive rate, we need to know the number of negatives
+  // and positives, respectively, in the true labels list
+  const numNegative = yTrue.filter(x => x === 0).length;
+  const numPositive = yTrue.filter(x => x === 1).length;
+
+  // Keep track of number of false positives and true positives. Initialize them with threshold 0
+  let fp = numNegative;
+  let tp = numPositive;
+
+  // List of fals positive rates and true positive rates. The false positive rate and true positive
+  // rate at all indices i form pairs
+  const fprs = [1];
+  const tprs = [1];
+
+  // Loop over all possible thresholds and calculate the tpr/fpr
+  sortedIndices.forEach((thresholdIndex) => {
+    if (yTrue[thresholdIndex] === 0) {
+      fp -= 1;
+    } else {
+      tp -= 1;
+    }
+
+    // Add the newly calculated tpr/fpr pair to the lists
+    fprs.push(fp / numNegative);
+    tprs.push(tp / numPositive);
+  });
+
+  // The area under the ROC curve is calculated by taking the area under each pair of points that
+  // follow each other on the x-axis. For each pair of points, the area under the trapezoid spanned
+  // by the points and the corresponding points on the x-axis is used as the area.
+
+  const fprsDiff = LinAlg.sum(
+    fprs.slice(0, -1),
+    LinAlg.scale(fprs.slice(1), -1),
+  );
+
+  const tprsMean = LinAlg.scale(
+    LinAlg.sum(
+      tprs.slice(0, -1),
+      tprs.slice(1),
+    ),
+    0.5
+  );
+
+  return fprsDiff.reduce((r, a, i) => r + a * tprsMean[i], 0);
 }
