fc276ead386dafa19171246622f8ab0d0513cb30
Jesper van Engelen <contact@jepps.nl>
2018-02-05 15:24:59 +0100

    #11 Add sigmoid kernel

--- src/kernel/index.js
+++ src/kernel/index.js
@@ -1,9 +1,11 @@
 import Linear from './linear';
 import Gaussian from './gaussian';
 import Polynomial from './polynomial';
+import Sigmoid from './sigmoid';
 
 export default {
   Linear,
   Gaussian,
   Polynomial,
+  Sigmoid,
 };
---
+++ src/kernel/sigmoid.js
@@ -0,0 +1,39 @@
+// Internal dependencies
+import Kernel from './base';
+import * as Arrays from '../arrays';
+
+/**
+ * The Sigmoid kernel. The formula of this kernel is as follows:
+ *   tanh(gamma * <x, y> + coef0)
+ */
+export default class SigmoidKernel extends Kernel {
+  /**
+   * Initialize the Sigmoid kernel with user-specified parameters
+   *
+   * @param {Object} [options] - User-defined options
+   * @param {number} [options.gamma = 1] - Normalization parameter of basic dot product
+   * @param {number} [options.coef0 = 1] - Bias coefficient (not part of the dot product)
+   */
+  constructor({ gamma = 1, coef0 = 1 } = {}) {
+    super();
+
+    /**
+     * Normalization parameter of basic dot product
+     * @type {number}
+     */
+    this.gamma = gamma;
+
+    /**
+     * Bias coefficient (not part of the dot product)
+     * @type {number}
+     */
+    this.coef0 = coef0;
+  }
+
+  /**
+   * @see {@link Kernel#apply}
+   */
+  apply(x, y) {
+    return Math.tanh(this.gamma * Arrays.dot(x, y) + this.coef0);
+  }
+}
