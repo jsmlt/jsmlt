efd9658d43ec8bbdf76ded5e8dcb82b02ff83229
Jesper van Engelen <contact@jepps.nl>
2018-11-02 20:49:15 -0400

    Fix includes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import Arrays from './arrays/';
-import Classification from './classification/';
-import Data from './data/';
-import * as Datasets from './datasets/';
-import Kernel from './kernel/';
-import * as ModelSelection from './model-selection/';
-import Preprocessing from './preprocessing/';
-import Random from './random/';
-import Supervised from './supervised/';
-import UI from './ui/';
-import Unsupervised from './unsupervised/';
-import Util from './util/';
-import Validation from './validation/';
+import Arrays from './arrays';
+import Classification from './classification';
+import Data from './data';
+import * as Datasets from './datasets';
+import Kernel from './kernel';
+import * as ModelSelection from './model-selection';
+import Preprocessing from './preprocessing';
+import Random from './random';
+import Supervised from './supervised';
+import UI from './ui';
+import Unsupervised from './unsupervised';
+import Util from './util';
+import Validation from './validation';
 
 export default {
   Arrays,
